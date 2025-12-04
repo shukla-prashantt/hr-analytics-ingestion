6784fe9d0992e0df4b85052bf889988f8c0add35
Prashant Shukla <prashant.shukla07214@gmail.com>
2025-12-04 18:21:37 +0530

    Ticket-1:Added JPA Entities

---
+++ src/main/java/com/fsdprojects/hr_analytics_ingestion/employee/entity/SalaryValue.java
@@ -0,0 +1,61 @@
+package com.fsdprojects.hr_analytics_ingestion.employee.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SalaryValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    private String currency;
+
+    private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
