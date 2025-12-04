119e9ec097b150cb2d18a883a1dd4c31e91d49b2
Prashant Shukla <prashant.shukla07214@gmail.com>
2025-12-04 18:11:24 +0530

    Initial Commit

---
+++ src/main/java/com/fsdprojects/hr_analytics_ingestion/HrAnalyticsIngestionApplication.java
@@ -0,0 +1,13 @@
+package com.fsdprojects.hr_analytics_ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAnalyticsIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAnalyticsIngestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsdprojects/hr_analytics_ingestion/HrAnalyticsIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdprojects.hr_analytics_ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAnalyticsIngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
